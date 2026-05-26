1f11fab45707fede450ec2d1213cb8bac1f55404
202611325-ship-it <2026_11325@kshcm.net>
2026-05-26 13:18:47 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
-# app.py
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 
 # -----------------------------------
